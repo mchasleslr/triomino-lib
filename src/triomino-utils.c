d8c3a79ff20819ac138f856060f77ebd31c6f586
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-03-03 16:19:43 +0100

    Add an utility module

---
+++ src/triomino-utils.c
@@ -0,0 +1,14 @@
+/**
+ * @file       triomino-utils.c
+ *
+ * @author     Maxime Chasles
+ * @date       2024
+ * @copyright  BSD 3-Clause License
+ */
+
+#include "triomino-utils.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "triomino-types.h"
---
+++ src/triomino-utils.h
@@ -0,0 +1,14 @@
+/**
+ * @file       triomino-utils.h
+ *
+ * @author     Maxime Chasles
+ * @date       2024
+ * @copyright  BSD 3-Clause License
+ */
+
+#ifndef _TRIOMINO_UTILS_H
+#define _TRIOMINO_UTILS_H
+
+#include "triomino-types.h"
+
+#endif // _TRIOMINO_UTILS_H
