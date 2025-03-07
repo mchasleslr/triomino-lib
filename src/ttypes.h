008e8a4debcaa7b3f98f4ac57258992b2e0ecfe8
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-03-07 09:05:03 +0100

    feat(types): add common types header
    Add ttypes.h:
    - Base types from triomino.inc
    - Common type aliases
    - Boolean support

---
+++ src/ttypes.h
@@ -0,0 +1,20 @@
+/**
+ * @file       ttypes.h
+ *
+ *             This module provides the types and aliases used in the project.
+ *
+ * @author     Maxime Chasles
+ * @date       2024-2025
+ * @copyright  BSD 3-Clause License
+ */
+
+// ----------------------------------------------------------------------------
+
+#ifndef _TTYPES_H
+#define _TTYPES_H
+
+#include <stdbool.h>
+
+#include "triomino.inc"
+
+#endif // _TTYPES_H
