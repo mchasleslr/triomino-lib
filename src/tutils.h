4e65232825df5652497d2ef1497912de4b93817f
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-03-07 09:00:15 +0100

    feat(utils): add general utility header
    Create tutils.h to centralize common utilities:
    - ARRAY_LEN: macro to get static array length

---
+++ src/tutils.h
@@ -0,0 +1,24 @@
+/**
+ * @file       tutils.h
+ *
+ *             This module provides general purpose utility functions.
+ *
+ * @author     Maxime Chasles
+ * @date       2024-2025
+ * @copyright  BSD 3-Clause License
+ */
+
+// ----------------------------------------------------------------------------
+
+#ifndef _TUTILS_H
+#define _TUTILS_H
+
+/**
+ * @brief Gets the length of an array.
+ *
+ * @param t The array.
+ * @return The length of the array.
+ */
+#define ARRAY_LEN(t) (sizeof(t) / sizeof(t[0]))
+
+#endif // _TUTILS_H
