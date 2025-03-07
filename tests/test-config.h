7f05d1929475c11ffb59d5dc82c7b4c7b54c1909
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-03-07 09:34:42 +0100

    feat(test): add minimal test infrastructure
    Add test header files:
    - test-config.h: Assert configuration
    - test.h: Test execution macros
    
    Features:
    - Force assertions during tests
    - Simple test execution macro
    - Lightweight test framework for CMake

---
+++ tests/test-config.h
@@ -0,0 +1,10 @@
+#ifndef _TEST_CONFIG_H
+#define _TEST_CONFIG_H
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+
+#include <assert.h>
+
+#endif // _TEST_CONFIG_H
---
+++ tests/test.h
@@ -0,0 +1,10 @@
+#ifndef _TEST_H
+#define _TEST_H
+
+#define RUN_TEST(test) \
+  do                   \
+  {                    \
+    test();            \
+  } while (0)
+
+#endif // _TEST_H
