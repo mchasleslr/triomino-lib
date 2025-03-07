10fd1c32697fd8cddec27bdf2d14a57435523272
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-03-07 09:02:36 +0100

    feat(memory): add memory management wrapper
    Create tmem module to centralize memory management:
    - Add t_malloc() wrapper for malloc()
    
    This simplifies memory error handling across the project.

---
+++ src/tmem.c
@@ -0,0 +1,36 @@
+/**
+ * @file       tmem.c
+ *
+ *             This module provides memory management functions.
+ *
+ * @author     Maxime Chasles
+ * @date       2024-2025
+ * @copyright  BSD 3-Clause License
+ */
+
+// ----------------------------------------------------------------------------
+
+#include "tmem.h"
+
+#include <stdlib.h>
+
+// ----------------------------------------------------------------------------
+
+/**
+ * @brief Allocates memory.
+ *
+ * @param size The size of the memory to allocate.
+ * @return A pointer to the allocated memory.
+ */
+void *t_malloc(size_t size)
+{
+  void *ptr;
+
+  if (NULL == (ptr = malloc(size)))
+  {
+    perror("malloc ");
+    exit(EXIT_FAILURE);
+  }
+
+  return ptr;
+}
---
+++ src/tmem.h
@@ -0,0 +1,18 @@
+/**
+ * @file       tmem.h
+ *
+ * @author     Maxime Chasles
+ * @date       2024-2025
+ * @copyright  BSD 3-Clause License
+ */
+
+// ----------------------------------------------------------------------------
+
+#ifndef _TMEM_H
+#define _TMEM_H
+
+#include "stddef.h"
+
+void *t_malloc(size_t size);
+
+#endif // _TMEM_H
