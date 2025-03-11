a286e19d948f22811a19073894628b925a46d962
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-03-11 16:45:36 +0100

    feat(rand): add random integer generation function
    Add t_rand_randint() to:
    - Generate random integers in a specified range
    - Provide simple min/max inclusive interface
    - Encapsulate random number generation logic
    - Initialize random seed automatically
    - Support testing with random values
    
    This makes random number generation consistent across the project.

---
+++ src/trand.h
@@ -0,0 +1,14 @@
+/**
+ * @file       trand.h
+ *
+ * @author     Maxime Chasles
+ * @date       2025
+ * @copyright  BSD 3-Clause License
+ */
+
+#ifndef _TRAND_H
+#define _TRAND_H
+
+extern int t_rand_randint(int min, int max);
+
+#endif  // _TRAND_H
